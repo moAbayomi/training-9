db124f9efeaaddaa10bf1745fd1151b80994e7ff
muhd <114200127+moAbayomi@users.noreply.github.com>
2024-01-26 12:46:06 +0100

    Add files via upload

---
+++ Thrift.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.19;
+
+contract Thrift {
+    uint256 public overallBal;
+
+    event Funded(uint256 indexed amount, address indexed funder);
+
+    event Withdrawn(uint256 indexed amount, address indexed withdrawer);
+
+    mapping(address => Participant) public addressToParticipant;
+
+    struct Participant {
+        uint256 amountThrifted;
+        uint256 time;
+    }
+
+    function fund() external payable {
+        require(msg.value > 0, "amount cant be zero, sorry");
+        addressToParticipant[msg.sender] = Participant(
+            msg.value,
+            block.timestamp
+        );
+        overallBal += msg.value;
+        emit Funded(msg.value, msg.sender);
+    }
+
+    function checkMyBal() external view returns (uint256) {
+        return addressToParticipant[msg.sender].amountThrifted;
+    }
+
+    function withdraw(uint amount) external returns (bool) {
+        uint256 balance = addressToParticipant[msg.sender].amountThrifted;
+        require(amount > 0 && balance >= amount, "lmao");
+
+        addressToParticipant[msg.sender].amountThrifted -= amount;
+        overallBal -= amount;
+
+        payable(msg.sender).transfer(amount);
+        emit Withdrawn(amount, msg.sender);
+        return true;
+    }
+}
